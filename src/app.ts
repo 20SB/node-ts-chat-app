1131d6e8abb5072714664e5c8b5fd8aa60e28d4f
SB-wstf <subha.biswal@thewasserstoff.com>
2025-02-03 18:51:22 +0530

    basic node ts server setup

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+import cors from 'cors';
+import router from './routes';
+
+const app = express();
+app.use(cors({ origin: '*' }));
+app.use(express.json());
+
+// app.use('/', router);
+
+const port = process.env.PORT || 3000;
+app.listen(port,()=>{
+    console.log('Server is running on port ',port);
+})
+
